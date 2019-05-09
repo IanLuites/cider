a434870c702a5205ed543840f151d76cb60441ce
Ian Luites <IanLuites@users.noreply.github.com>
2019-05-09 22:50:29 +0200

    Bump version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cider.Mixfile do
   def project do
     [
       app: :cider,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.8",
       description: "CIDR library for Elixer.",
       package: package(),
